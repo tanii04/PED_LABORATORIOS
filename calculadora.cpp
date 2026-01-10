cf2e42a64b178ec9725a8f0fe91899d96b68066c
tanii04 <00385125@uca.edu.sv>
2026-01-09 22:21:17 -0600

    add: Se añadio el menu de la calculadora

---
+++ calculadora.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    int opcion;
+    double num1, num2;
+
+    do{
+
+        cout << endl << "CALCULADORA" << endl << endl;
+        cout << "1. Sumar" << endl;
+        cout << "2. Restar" << endl;
+        cout << "3. Multiplicar" << endl;
+        cout << "4. Dividir" << endl;
+        cout << "5. Salir" << endl << endl;
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        
+        if(opcion >= 1 && opcion <= 4){
+            cout << endl << "Ingrese el primer numero: ";
+            cin >> num1;
+            cout << "Ingrese el segundo numero: ";
+            cin >> num2;
+        }
+    }
+
+    return 0;
+}
