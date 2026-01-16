efac47ab463e579ad752d3c4557627793b82899c
pam927-stph <00383825@uca.edu.sv>
2026-01-16 17:28:54 -0600

    se agrego la parte de listas enlazada

--- Labo 2/listadeestudiantes.cpp
+++ Labo 2/listadeestudiantes.cpp
@@ -21,9 +21,62 @@ class estudiante{
     }
 };
 
+ class ListaEnlazada {
+private:
+    estudiante* head;
+    float sumaNotas;
+
+    
+    int contarRecursivo(estudiante* actual) {
+        if (actual == nullptr) {
+            return 0; 
+        }
+        return 1 + contarRecursivo(actual->siguiente);
+    }
+
+public:
+    ListaEnlazada() {
+        head = nullptr;
+        sumaNotas = 0;
+    }
+
+    
+    void agregarEstudiante(string nombre, float nota) {
+        estudiante* nuevo = new estudiante(nombre, nota);
+        sumaNotas += nota;
+
+        if (head == nullptr) {
+            head = nuevo;
+        } else {
+            estudiante* temp = head;
+            while (temp->siguiente != nullptr) {
+                temp = temp->siguiente;
+            }
+            temp->siguiente = nuevo;
+        }
+    }
+
+  
+    void imprimirLista() {
+        estudiante* temp = head;
+        while (temp != nullptr) {
+            temp->imprimir();
+            temp = temp->siguiente;
+        }
+    }
+
+    
+    float calcularPromedioRecursivo() {
+        int cantidad = contarRecursivo(head);
+        if (cantidad == 0) {
+            return 0;
+        }
+        return sumaNotas / cantidad;
+    }
+};
+
 
 int main(void){
-
 
 
 
