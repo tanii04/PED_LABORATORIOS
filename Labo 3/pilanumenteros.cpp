7b97fce6b4cf825e5a644fe21208405da0fae3e9
tanii04 <00385125@uca.edu.sv>
2026-01-23 16:38:12 -0600

    add:Se agrego la funcionalidad eliminar tope (pop)

--- Labo 3/pilanumenteros.cpp
+++ Labo 3/pilanumenteros.cpp
@@ -16,6 +16,17 @@ void push(nodo*& tope, int num){
     cout << "Su numero se ha agregado a la pila" << endl;
 }
 
+void pop(nodo*& tope){
+    if(tope == NULL){
+        cout << "No hay elementos que eliminar" << endl;
+        return;
+   }
+
+   nodo* aux = tope;
+   tope = tope->siguiente;
+   delete aux;
+   cout << "Su elemento se ha eliminado de la pila" << endl;
+}
 
 int main(void){
 
